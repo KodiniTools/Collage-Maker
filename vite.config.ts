2da1ec877e0332794069a4c6b913ca364afec812
Dinko Ramić <ramic.dinko@gmail.com>
2025-11-06 01:23:05 +0100

    Change base path to use environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/collagen/',
+  base: process.env.VITE_BASE_PATH || '/collagemaker/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
